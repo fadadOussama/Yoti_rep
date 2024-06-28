aff09a5bf3caec8e61e22d23f788a62a3f491977
Fadad Oussama <fadadoussama@gmail.com>
2024-06-28 13:18:17 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -62,14 +62,17 @@ module.exports = {
       template: "public/index.html",
       favicon: "public/favicon.png",
     }),
-    // new CopyPlugin({
-    //   patterns: [
-    //     {
-    //       from: "./node_modules/@getyoti/react-face-capture/assets",
-    //       to: "./assets",
-    //     },
-    //   ],
-    // }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(
+            __dirname,
+            "./node_modules/@getyoti/react-face-capture/assets"
+          ),
+          to: path.resolve(__dirname, "./dist/assets"),
+        },
+      ],
+    }),
     new Dotenv({
       path: "./.env.local",
     }),
